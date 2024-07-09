49d3eee2d6929f12581896d9fff4900255e19c4a
Roman Mishchenko <mishchenkor@gmail.com>
2024-07-09 10:41:30 +0300

    Firs commit

---
+++ loki/prom_loki.sh
@@ -0,0 +1,171 @@
+#!/bin/bash
+
+# Перевірка чи встановлений Docker
+if ! [ -x "$(command -v docker)" ]; then
+  echo "Docker не встановлений. Встановлення Docker..."
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sh get-docker.sh
+  rm get-docker.sh
+  sudo systemctl start docker
+  sudo systemctl enable docker
+else
+  echo "Docker вже встановлений."
+fi
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo "Docker Compose не встановлений. Встановлення Docker Compose..."
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+else
+  echo "Docker Compose вже встановлений."
+fi
+
+# Створення .env файлів для Promtail та Loki
+# cat <<EOF > .env_loki
+# LOKI_PORT=3100
+# LOKI_LOG_LEVEL=info
+# LOKI_CONFIG_FILE=/etc/loki/config.yml
+# LOKI_SERVER_HTTP_LISTEN_PORT=${LOKI_PORT}
+# LOKI_TABLE_MANAGER_RETENTION_PERIOD=24h
+# EOF
+# cat <<EOF > .env_promtail
+# PROMTAIL_CONFIG_FILE=/etc/promtail/promtail.yml
+# PROMTAIL_POSITIONS_FILE=/tmp/positions.yaml
+# PROMTAIL_CLIENT_URL=http://localhost:${LOKI_PORT}/loki/api/v1/push
+# PROMTAIL_LOG_LEVEL=info
+# EOF
+source .env_loki
+
+cat <<EOF > promtail-config.yml
+server:
+  http_listen_port: 9080
+  grpc_listen_port: 0
+
+positions:
+  filename: /tmp/positions.yaml
+
+clients:
+  - url: http://loki:${LOKI_PORT}/loki/api/v1/push
+    batchwait: 5s
+    batchsize: 102400
+
+scrape_configs:
+  - job_name: varlogs
+    static_configs:
+      - targets:
+          - localhost
+        labels:
+          job: varlogs
+          __path__: /var/log/messages/*
+
+    pipeline_stages:
+      - match:
+          selector: '{job="varlogs"}'
+          stages:
+            - regex:
+                expression: '.*(ERROR|WARN|CRITICAL).*'
+            - timestamp:
+                source: time
+                format: RFC3339
+            - labels:
+                level: level
+            - output:
+                source: log
+                format: logfmt
+                fields:
+                  time: timestamp
+                  path: filename
+                  log: message
+
+EOF
+
+cat <<EOF > loki-config.yml
+auth_enabled: false
+
+server:
+  http_listen_port: ${LOKI_PORT}
+
+ingester:
+  lifecycler:
+    address: 127.0.0.1
+    ring:
+      kvstore:
+        store: inmemory
+      replication_factor: 1
+  chunk_idle_period: 3m
+  chunk_retain_period: 1m
+
+schema_config:
+  configs:
+  - from: 2020-10-24
+    store: boltdb-shipper
+    object_store: filesystem
+    schema: v11
+    index:
+      prefix: index_
+      period: 24h
+
+storage_config:
+  boltdb_shipper:
+    active_index_directory: /loki/index
+    cache_location: /loki/cache
+    cache_ttl: 24h
+    shared_store: filesystem
+  filesystem:
+    directory: /loki/chunks
+
+compactor:
+  working_directory: /loki/compactor
+  shared_store: filesystem
+
+limits_config:
+  enforce_metric_name: false
+  reject_old_samples: true
+  reject_old_samples_max_age: 168h
+  ingestion_rate_mb: 10
+  ingestion_burst_size_mb: 20
+  max_entries_limit_per_query: 5000
+  max_global_streams_per_user: 10000
+
+
+
+chunk_store_config:
+  max_look_back_period: 0s
+
+table_manager:
+  retention_deletes_enabled: false
+  retention_period: 0s
+EOF
+
+# Створення docker-compose.yml
+cat <<EOF > docker-compose.yml
+version: '3.8'
+
+services:
+  loki:
+    image: grafana/loki:latest
+    container_name: loki
+    restart: unless-stopped
+    ports:
+      - "${LOKI_PORT}:3100"
+    volumes:
+      - ./loki-config.yml:/etc/loki/config.yml
+    env_file:
+      - .env_loki
+
+  promtail:
+    image: grafana/promtail:latest
+    container_name: promtail
+    volumes:
+      - ./promtail-config.yml:/etc/promtail/promtail.yml
+      - /var/log:/var/log
+    command:
+      - '-config.file=/etc/promtail/promtail.yml'
+    env_file:
+      - .env_promtail
+
+EOF
+
+# Запуск docker-compose
+docker-compose up -d
+
---
+++ promet/prom.sh
@@ -0,0 +1,225 @@
+#!/bin/bash
+
+# Перевірка чи встановлений Docker
+if ! [ -x "$(command -v docker)" ]; then
+  echo "Docker не встановлений. Встановлення Docker..."
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sh get-docker.sh
+  rm get-docker.sh
+  sudo systemctl start docker
+  sudo systemctl enable docker
+else
+  echo "Docker вже встановлений."
+fi
+source .env_pro
+if ! [ -x "$(command -v docker-compose)" ]; then
+  echo "Docker Compose не встановлений. Встановлення Docker Compose..."
+  sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+  sudo chmod +x /usr/local/bin/docker-compose
+  sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+else
+  echo "Docker Compose вже встановлений."
+fi
+
+# Наступний файл для  Налаштування правил сповіщень у Prometheus
+cat <<EOF > alerts.yml
+groups:
+  - name: example
+    rules:
+      - alert: InstanceDown
+        expr: up == 0
+        for: 5m
+        labels:
+          severity: critical
+        annotations:
+          summary: "Instance {{ $labels.instance }} down"
+          description: "{{ $labels.instance }} of job {{ $labels.job }} has been down for more than 5 minutes."
+
+  - name: example_log
+    rules:
+      - alert: CriticalLogs
+        expr: sum(rate({job="varlogs", level="critical"}[5m])) > 0
+        for: 5m
+        labels:
+          severity: critical
+        annotations:
+          summary: "Critical log entries detected"
+          description: "There are critical log entries in the last 5 minutes."
+
+  - name: example_cpu
+    rules:
+      - alert: HighCPULoad
+        expr: 100 - (avg by (instance) (irate(node_cpu_seconds_total{mode="idle"}[5m])) * 100) > 80
+        for: 1m
+        labels:
+          severity: warning
+        annotations:
+          summary: "High CPU load on {{ $labels.instance }}"
+          description: "CPU load is above 80% on {{ $labels.instance }}."
+
+  - name: example_info_test
+    rules:
+      - alert: TestAlert
+        expr: vector(1)  # Це правило завжди буде спрацьовувати
+        for: 1m
+        labels:
+          severity: info
+        annotations:
+          summary: "Test Alert"
+          description: "This is a test alert for verifying alerting setup."
+EOF
+
+# Створення entrypoint для Prometheus 
+cat <<EOF > entrypoint.sh
+#!/bin/sh
+
+# Вивести повідомлення для діагностики
+echo "Starting entrypoint script..."
+
+# # Завантажити змінні з .env_pro файлу
+# if [ -f /etc/prometheus/.env_pro ]; then
+#     echo "Loading environment variables from .env_pro"
+export $(grep -v '^#' /etc/prometheus/.env_pro | xargs)
+# else
+#     echo ".env_pro file not found!"
+#     exit 1
+# fi
+
+# Вивести значення змінних для діагностики
+echo "ALERTMANAGER_IP=${ALERTMANAGER_IP}"
+echo "ALERTMANAGER_PORT=${ALERTMANAGER_PORT}"
+echo "PROMETHEUS_IP=${PROMETHEUS_IP}"
+echo "PROMETHEUS_PORT=${PROMETHEUS_PORT}"
+echo "NODE_EXP_IP=${NODE_EXP_IP}"
+echo "NODE_EXP_PORT=${NODE_EXP_PORT}"
+echo "LOKI_IP=${LOKI_IP}"
+echo "LOKI_PORT=${LOKI_PORT}"
+
+# Обробити шаблонний файл і згенерувати prometheus.yml
+envsubst < /etc/prometheus/prometheus.yml.tpl > /etc/prometheus/prometheus.yml
+
+# Вивести вміст згенерованого файлу для перевірки
+echo "Generated prometheus.yml:"
+cat /etc/prometheus/prometheus.yml
+
+# Запустити Prometheus
+prometheus --config.file=/etc/prometheus/prometheus.yml
+EOF
+
+# Створення конфігураційного файлу Prometheus 
+cat <<EOF > prometheus.yml.tpl
+global:
+  scrape_interval: 1m
+  scrape_timeout: 10s
+  evaluation_interval: 1m
+
+alerting:
+  alertmanagers:
+    - static_configs:
+        - targets: 
+            - ${ALERTMANAGER_IP}:${ALERTMANAGER_PORT}
+
+rule_files:
+  - /etc/prometheus/alerts.yml
+
+scrape_configs:
+  - job_name: prometheus
+    honor_timestamps: true
+    scrape_interval: 1m
+    scrape_timeout: 10s
+    metrics_path: /metrics
+    scheme: http
+    follow_redirects: true
+    static_configs:
+      - targets:
+          - ${PROMETHEUS_IP}:${PROMETHEUS_PORT}
+
+  - job_name: node_exporter
+    honor_timestamps: true
+    scrape_interval: 1m
+    scrape_timeout: 10s
+    metrics_path: /metrics
+    scheme: http
+    follow_redirects: true
+    static_configs:
+      - targets: 
+          - ${NODE_EXP_IP}:${NODE_EXP_PORT}
+
+  - job_name: loki
+    honor_timestamps: true
+    scrape_interval: 1m
+    scrape_timeout: 10s
+    metrics_path: /metrics
+    scheme: http
+    follow_redirects: true
+    static_configs:
+      - targets: 
+          - ${LOKI_IP}:${LOKI_PORT}
+
+  - job_name: alertmanager
+    static_configs:
+      - targets: 
+          - ${ALERTMANAGER_IP}:${ALERTMANAGER_PORT}
+EOF
+
+# mv entrypoint entrypoint.sh
+chmod +x entrypoint.sh
+
+cat <<EOF > Dockerfile
+FROM alpine:latest
+
+RUN apk add --no-cache \
+    prometheus \
+    gettext
+
+COPY prometheus.yml.tpl /etc/prometheus/prometheus.yml.tpl
+COPY alerts.yml /etc/prometheus/alerts.yml
+COPY entrypoint.sh /etc/prometheus/entrypoint.sh
+COPY .env_pro /etc/prometheus/.env_pro
+
+ENTRYPOINT ["/bin/sh", "/etc/prometheus/entrypoint.sh"]
+EOF
+
+# Створення docker-compose.yml
+cat <<EOF > docker-compose.yml
+version: '3.8'
+
+services:
+  prometheus:
+    build: .
+    container_name: prometheus
+    restart: always
+    volumes:
+      - prometheus_data:/prometheus
+      - ./prometheus.yml.tpl:/etc/prometheus/prometheus.yml.tpl
+      - ./alerts.yml:/etc/prometheus/alerts.yml
+      - ./entrypoint.sh:/entrypoint.sh
+      - ./.env_pro:/etc/prometheus/.env_pro
+    ports:
+      - "${PROMETHEUS_PORT}:9090"
+    env_file:
+      - .env_pro
+    command:
+      # - '--config.file=/etc/prometheus/prometheus.yml'
+      - '--storage.tsdb.path=/prometheus'
+      - '--web.enable-lifecycle'
+      - '--web.listen-address=0.0.0.0:${PROMETHEUS_PORT}'
+
+  node_exporter:
+    image: quay.io/prometheus/node-exporter:latest
+    container_name: node_exporter
+    pid: host
+    restart: always
+    ports:
+      - "${NODE_EXP_PORT}:9100"
+    volumes:
+      - '/:/host:ro,rslave'
+    env_file:
+      - .env_node
+
+volumes:
+  prometheus_data:
+EOF
+
+# Запуск docker-compose
+ docker-compose up -d --build
